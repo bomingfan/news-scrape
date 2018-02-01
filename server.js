6de65ac73d94c9765b2911715fc27514c7dfb75f
buzzhaha <boming.fan@gmail.com>
2018-02-01 10:06:51 -0800

    mid

--- server.js
+++ server.js
@@ -81,26 +81,24 @@ app.get("/scrape", function(req, res) {
     .catch(function(err) {
       // If an error occurred, send it to the client
       return res.status(400).json(err);
+      
     });
-
-    // If we were able to successfully scrape and save an Article, send a message to the client
-    // res.json("Scrape Complete");
   });
 });
 
 // Route for getting all Articles from the db
-// app.get("/articles", function(req, res) {
-//   // Grab every document in the Articles collection
-//   db.Article.find({})
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find Articles, send them back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.render("news", {news: dbArticle});
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 // // Route for grabbing a specific Article by id, populate it with it's note
 // app.get("/articles/:id", function(req, res) {
